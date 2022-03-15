fd5e0a2b899d38382a8316ca499388d99e658c50
marialovgren <marialovgren86@hotmail.com>
2022-03-15 15:06:32 +0100

    added favicons for user-avatar

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -3,7 +3,21 @@
  */
 
 
-/** Hello this is Julia from juliaj branch */
+// update user list
+const updatePlayerList = players => {
+	document.querySelector('#online-players').innerHTML = 
+		Object.values(players).map(username => `<li><span class="fa-solid fa-user-astronaut"></span>${username}<li>`).join("");
+}
 
-/** This is a test */
+// <i class="fa-solid fa-user-bounty-hunter"></i>
+// <i class="fa-solid fa-user-cowboy"></i>
+// <i class="fa-solid fa-user-crown"></i>
+// <i class="fa-solid fa-user-ninja"></i>
+// <i class="fa-solid fa-user-police"></i>
+// <i class="fa-solid fa-user-robot"></i>
+// <i class="fa-solid fa-alien-8bit"></i>
+// <i class="fa-solid fa-pinata"></i>
+// <i class="fa-solid fa-user-secret"></i>
 
+// * virus icon
+// <i class="fa-solid fa-virus-covid"></i>
