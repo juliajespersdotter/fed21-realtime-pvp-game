890ec99958323e7e234280e416f9589a04befd58
Julia Nemell <jnemell@gmail.com>
2022-03-16 17:13:54 +0100

    add a dropdown menu with avatars, starting page

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -89,16 +89,7 @@ createBoard(gameGrid);
 // const socket = io();
 
 
-// <i class="fa-solid fa-user-astronaut"></i>
-// <i class="fa-solid fa-user-bounty-hunter"></i>
-// <i class="fa-solid fa-user-cowboy"></i>
-// <i class="fa-solid fa-user-crown"></i>
-// <i class="fa-solid fa-user-ninja"></i>
-// <i class="fa-solid fa-user-police"></i>
-// <i class="fa-solid fa-user-robot"></i>
-// <i class="fa-solid fa-alien-8bit"></i>
-// <i class="fa-solid fa-pinata"></i>
-// <i class="fa-solid fa-user-secret"></i>
+
 
 // * virus icon
 // <i class="fa-solid fa-virus-covid"></i>
