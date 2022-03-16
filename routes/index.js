1756a15aaa4b3cde2d2b4367a8afb512566f0277
Julia Nemell <jnemell@gmail.com>
2022-03-16 11:08:19 +0100

    forEach loop to show users in game. added in players.ejs

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@
 const express = require('express');
 const router = express.Router();
 
+//add value from when player enters a username to username. 
 router.get('/', (req, res) => {
 	const players = [
 		{
-		username: username, 
-		room: room
+		username: "Julia"
 		}
 	]
 
