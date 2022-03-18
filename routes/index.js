72dcb11c7dfd9f832142b3c6cdfa696e8bd117ff
Julia <79936455+juliajespersdotter@users.noreply.github.com>
2022-03-18 10:09:23 +0100

    Testing deployment to heroku.

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,7 @@ const router = express.Router();
 
 //add value from when player enters a username to username. 
 router.get('/', (req, res) => {
-	const players = [
-		{
-		username: "Julia"
-		}
-	]
+	
 	console.log(players.username)
 	res.render('index', { players });
 });
