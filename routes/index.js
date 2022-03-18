3e77e564f87e33386a0d03d5c854aa3504d85886
marialovgren <marialovgren86@hotmail.com>
2022-03-18 14:45:34 +0100

    removed all EJS code including all ejs-documents and cod refering to ejs

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const router = express.Router();
 
 //add value from when player enters a username to username. 
+/* KOMMENTERAT BORT EFTERSOM VI INTE ANVÄNDER EJS LÄNGRE
 router.get('/', (req, res) => {
 	const players = [
 		{
@@ -15,5 +16,6 @@ router.get('/', (req, res) => {
 	console.log(players.username)
 	res.render('index', { players });
 });
+*/
 
 module.exports = router;
